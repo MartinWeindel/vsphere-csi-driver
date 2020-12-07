2957880f23c1cdf6920d2e0016ad8c9e1f170298
Martin Weindel <martin.weindel@sap.com>
2020-12-07 18:06:15 +0100

    fix node not existing

--- pkg/csi/service/vanilla/controller.go
+++ pkg/csi/service/vanilla/controller.go
@@ -35,6 +35,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	"sigs.k8s.io/vsphere-csi-driver/pkg/apis/migration"
+	cnsnode "sigs.k8s.io/vsphere-csi-driver/pkg/common/cns-lib/node"
 	cnsvolume "sigs.k8s.io/vsphere-csi-driver/pkg/common/cns-lib/volume"
 	cnsvsphere "sigs.k8s.io/vsphere-csi-driver/pkg/common/cns-lib/vsphere"
 	cnsconfig "sigs.k8s.io/vsphere-csi-driver/pkg/common/config"
@@ -729,6 +730,10 @@ func (c *controller) ControllerUnpublishVolume(ctx context.Context, req *csi.Con
 	// Block Volume
 	node, err := c.nodeMgr.GetNodeByName(ctx, req.NodeId)
 	if err != nil {
+		if err == cnsnode.ErrNodeNotFound {
+			// node is not existing anymore
+			return &csi.ControllerUnpublishVolumeResponse{}, nil
+		}
 		msg := fmt.Sprintf("failed to find VirtualMachine for node:%q. Error: %v", req.NodeId, err)
 		log.Error(msg)
 		return nil, status.Error(codes.Internal, msg)
